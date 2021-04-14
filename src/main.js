2e6ea5c39b2939bfff85d30aa715d462698175cf
christofersierra <christofer.sierra@sicom.cl>
2021-04-14 16:08:08 -0400

    Install dependencys

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify/lib";
+import EvaIcons from "vue-eva-icons";
+Vue.config.productionTip = false;
+Vue.use(EvaIcons);
+Vue.use(Vuetify);
+const opts = {
+  icons: {
+    iconfont: "mdi",
+  },
+};
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify: new Vuetify(opts),
+  render: (h) => h(App),
+}).$mount("#app");
